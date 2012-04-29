e6bfc858824ccc4dbe1fe0e2974c27bf0e2dd980
Soumya Deb <debloper@gmail.com>
2012-04-29 16:43:25 +0530

    Push the available locale into the options

--- media/js/locale.js
+++ media/js/locale.js
@@ -12,14 +12,27 @@
 
 ( function () {
 
-    var selector = document.createElement("select");
-    selector.setAttribute("id", "tabzilla-locale");
-    selector.setAttribute("title", "Choose your language for Tabzilla");
+    Tabzilla.l10n = { // The entire localization string tree
 
-    selector.innerHTML = '<option value="en-US">Show Tabzilla in...</option>';
+        "all"   : { // List of all available locale
+            "en-US":    "English",
+        },
+
+    };
+
+    // Build the DOM for the locale-selector
+    Tabzilla.l10n.selector = document.createElement("select");
+    Tabzilla.l10n.selector.setAttribute("id", "tabzilla-locale");
+    Tabzilla.l10n.selector.setAttribute("title", "Choose your language for Tabzilla");
+
+    // Insert the <option> elements into the locale-selector
+    Tabzilla.l10n.selector.innerHTML = '<option value="en-US">Show Tabzilla in...</option>';
+    for (var i in Tabzilla.l10n.all) {
+          Tabzilla.l10n.selector.innerHTML += '<option value="' + i + '">'+ Tabzilla.l10n.all[i] +'</option>';
+    }
 
     jQuery(document).ready(function() {
         // Push the select-element into the DOM
-        document.getElementById("tabzilla-search").appendChild(selector);
+        document.getElementById("tabzilla-search").appendChild(Tabzilla.l10n.selector);
     });
 })();
