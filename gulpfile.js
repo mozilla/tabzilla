55c33a14ce5d1b057808e48555ed1e1dbc00bed2
schalkneethling <sneethling@mozilla.com>
2015-07-11 16:26:59 +0200

    First commit of the new tabzilla

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    watch = require('gulp-watch'),
+    del = require('del'),
+    path = require('path');
+
+gulp.task('less', function() {
+    return gulp.src('less/*.less')
+        .pipe(less({
+            paths: [path.join(__dirname, 'less', 'libs')]
+        }))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', ['less'], function() {
+    gulp.watch('less/**/*.less', ['less']);
+});
+
+gulp.task('dist', function() {
+    del('css');
+    gulp.start('less');
+});
+
+gulp.task('default', ['watch'], function() {
+    console.log('Watching all LESS files for changes...');
+})
